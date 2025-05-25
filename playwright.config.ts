475e6fd9da1a5d9665727250af5191f86592184e
Sathish Naidu Lingutla <sathish@Mac.lan>
2025-05-25 23:09:17 +0530

    initial commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
+//Checking
 
 /**
  * See https://playwright.dev/docs/test-configuration.
